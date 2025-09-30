9d0be2a6c2f3a0adb64cb36e4be2431731078822
Houssamdevv <HE133594@students.ephec.be>
2025-09-30 14:16:39 +0200

    Exercice 1 terminé

--- exercise1.mjs
+++ exercise1.mjs
@@ -9,11 +9,28 @@
 // Expected result:
 // [ 'Mobile phone', 'Laptop computer', 'Headphones', 'Smartwatch' ]
 
-const products = [
-  "  mobile phone ",
-  " laptop computer  ",
-  "Headphones",
-  "  smartwatch",
+const users = [
+  { id: 1, name: "Alice", age: 28, specialty: "Marketing" },
+  { id: 2, name: "Bob", age: 35, specialty: "Engineering" },
+  { id: 3, name: "Charlie", age: 29, specialty: "Marketing" },
+  { id: 4, name: "David", age: 42, specialty: "Engineering" },
+  { id: 5, name: "Eve", age: 29, specialty: "Design" },
 ];
+
+
+const userNames = users.map((user) => user.name);
+console.log("userNames =", userNames);
+
+
+const engineers = users.filter((user) => user.specialty === "Engineering");
+console.log("engineers =", engineers);
+
+
+const totalAge = users.reduce((sum, user) => sum + user.age, 0);
+const averageAge = totalAge / users.length;
+console.log("averageAge =", averageAge);
+
+const userDescriptions = users.map((user) => `${user.name} is ${user.age} years old.`);
+console.log("userDescriptions =", userDescriptions);
 
 // Your code here
